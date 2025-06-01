22af3664cb8683fd1458e6f725708cf56be7f45d
Mireau-Jules <saintlouismireaujules@gmail.com>
2025-06-01 13:24:22 -0400

    Complete Lab

--- main.js
+++ main.js
@@ -3,9 +3,25 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.getElementById("modal");
+const modalMessage = document.getElementById("modal-message")
+const hearts = document.querySelectorAll(".like-glyph");
+hearts.forEach(heart => heart.addEventListener("click", ()=> {
+  mimicServerCall()
+  .then(()=>{
+    heart.textContent = heart.textContent === EMPTY_HEART ? FULL_HEART : EMPTY_HEART;
+    heart.classList.toggle("activated-heart");
+  } )
+  .catch(error => {
+    modal.textContent = error;
+    modal.classList.remove("hidden");
+    setTimeout(()=> modal.classList.add("hidden"), 3000)
+  })
+
+}))
 
 
-
+   
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
